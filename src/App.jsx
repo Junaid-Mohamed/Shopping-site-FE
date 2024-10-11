299da129aea30716f15bacd0c5fdb70939f62dec
Mohammed Junaid <junaid7mohamed@gmail.com>
2024-10-11 11:55:54 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import reactLogo from './assets/react.svg'
+import Home from './pages/Home'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+import ReactDom from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
+import ProductListing from './pages/ProductsListing.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>
+  },
+  {
+    path: "/products",
+    element: <ProductListing/>
+  }
+])
+
+ReactDom.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
