781f310845406c72c7b1603767f7db2c7ccf5f92
Mohammed Junaid <junaid7mohamed@gmail.com>
2025-01-09 11:59:42 +0530

    feat: added login and signup img

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,6 @@ import { useAuth } from '../auth/AuthProvider';
 const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
-
   const { login } = useAuth();
 
   const from = location.state?.from || '/';
@@ -23,7 +22,7 @@ const Login = () => {
         'https://grocer-ease-five.vercel.app/api/users/login',
         formData
       );
-      if (data.status == 200) {
+      if (data.status === 200) {
         console.log(data);
         setSuccessMsg('Logged in successfully.');
         setTimeout(() => {
@@ -33,19 +32,13 @@ const Login = () => {
         }, 2000);
       }
     } catch (error) {
-      // console.log("Inside error");
       setSuccessMsg(error.response.data.message);
-      setFormData((prevState) => ({
-        email: '',
-        password: '',
-      }));
-      // console.log(error.response.data.message);
+      setFormData({ email: '', password: '' });
     }
   };
 
   const handleFormDataChange = (e) => {
     const { name, value } = e.target;
-
     setFormData((prevState) => ({
       ...prevState,
       [name]: value,
@@ -53,64 +46,66 @@ const Login = () => {
   };
 
   return (
-    <div className="container m-4 p-4 w-50">
-      <h1 className="my-4"> Login</h1>
-
-      <div className="my-4">
-        <div className="card">
-          <div className="card-header">
-            Test details to test the application functionlaity
+    <div className="container d-flex align-items-center justify-content-center min-vh-100">
+      <div className="row w-100">
+        <div className="col-md-6">
+          <h1 className="my-4">Login</h1>
+          <div className="my-4">
+            <div className="card">
+              <div className="card-header">
+                Test details to test the application functionality
+              </div>
+              <div className="card-body">
+                <p className="card-text">email: test@gmail.com</p>
+                <p className="card-text">password: TestPass</p>
+                <hr />
+                <p className="btn btn-info">
+                  Note: If you wish to signup you can go to signup page.
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="card-body">
-            <p className="card-text">email: test@gmail.com</p>
-            <p className="card-text">password: TestPass</p>
-            <hr />
-            <p className="btn btn-info">
-              Note: If you wish to signup you can go to signup page.
-            </p>
-          </div>
+          <form onSubmit={handleFormSubmit}>
+            <div className="form-group">
+              <label>Email address</label>
+              <input
+                type="email"
+                onChange={handleFormDataChange}
+                className="form-control"
+                name="email"
+                value={formData.email}
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group mt-4">
+              <label>Password</label>
+              <input
+                type="password"
+                onChange={handleFormDataChange}
+                className="form-control"
+                name="password"
+                value={formData.password}
+                placeholder="Password"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary my-4">
+              Login
+            </button>
+          </form>
+          <Link to={'/signup'} style={{ textDecoration: 'none' }}>
+            Don't have an account?{' '}
+            <span style={{ textDecoration: 'underline' }}>Sign Up</span>
+          </Link>
+          <div className="mt-3">{successMsg}</div>
+        </div>
+        <div className="col-md-6 d-none d-md-flex align-items-center justify-content-center">
+          <img
+            src="https://media.wired.com/photos/61562228a62d729110ec10fe/4:3/w_1766,h_1324,c_limit/Gear-Meal-Kits-misfits-market-source-misfits-market.jpeg"
+            alt="Login Illustration"
+            className="img-fluid rounded"
+          />
         </div>
       </div>
-
-      <form>
-        <div className="form-group">
-          <label>Email address</label>
-          <input
-            type="email"
-            onChange={handleFormDataChange}
-            className="form-control"
-            id="exampleInputEmail1"
-            name="email"
-            value={formData.email}
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="form-group mt-4">
-          <label>Password</label>
-          <input
-            type="password"
-            onChange={handleFormDataChange}
-            className="form-control"
-            id="exampleInputPassword1"
-            name="password"
-            value={formData.password}
-            placeholder="Password"
-          />
-        </div>
-        <button
-          type="submit"
-          onClick={handleFormSubmit}
-          className="btn btn-primary my-4"
-        >
-          Login
-        </button>
-      </form>
-      <Link to={'/signup'} style={{ textDecoration: 'none' }}>
-        Don't have an account?{' '}
-        <span style={{ textDecoration: 'underline' }}>Sign Up</span>
-      </Link>
-      <div className="mt-3">{successMsg}</div>
     </div>
   );
 };
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -25,7 +25,7 @@ const Signup = () => {
     phoneNumber: '',
   });
 
-  //   regex for email and phonenumber
+  // regex for email and phone number
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const phoneRegex = /^[0-9]{10}$/;
 
@@ -73,13 +73,11 @@ const Signup = () => {
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     if (validate()) {
-      //   console.log('FormData submitted.', formData);
       const data = await axios.post(
         'https://grocer-ease-five.vercel.app/api/users/',
         formData
       );
       setSuccessMsg('Signed up successfully. Now you can login.');
-      //   console.log(data);
       setTimeout(() => {
         setSuccessMsg('');
         navigate('/login');
@@ -90,115 +88,125 @@ const Signup = () => {
   };
 
   return (
-    <div className="container m-4 p-4 w-50">
-      <h1 className="my-4"> Sign Up</h1>
-      <form>
-        <div className="form-group">
-          <label>Name</label>
-          <input
-            type="text"
-            onChange={handleFormChange}
-            value={formData.name}
-            className="form-control"
-            name="name"
-            placeholder="Name"
-          />
-          {errors.name && <p style={{ color: 'red' }}>{errors.name}</p>}
+    <div className="container m-4 p-4">
+      <h1 className="my-4">Sign Up</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <form>
+            <div className="form-group">
+              <label>Name</label>
+              <input
+                type="text"
+                onChange={handleFormChange}
+                value={formData.name}
+                className="form-control"
+                name="name"
+                placeholder="Name"
+              />
+              {errors.name && <p style={{ color: 'red' }}>{errors.name}</p>}
+            </div>
+            <div className="form-group mt-3">
+              <label>Email</label>
+              <input
+                type="email"
+                onChange={handleFormChange}
+                className="form-control"
+                name="email"
+                value={formData.email}
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
+              <small id="emailHelp" className="form-text text-muted">
+                We'll never share your email with anyone else.
+              </small>
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                onChange={handleFormChange}
+                value={formData.password}
+                className="form-control"
+                name="password"
+                placeholder="Password"
+              />
+              {errors.password && <p style={{ color: 'red' }}>{errors.password}</p>}
+            </div>
+            <div className="form-group mt-3">
+              <label>Phone Number</label>
+              <input
+                type="text"
+                onChange={handleFormChange}
+                value={formData.phoneNumber}
+                className="form-control"
+                name="phoneNumber"
+                placeholder="Phone"
+              />
+              {errors.phoneNumber && (
+                <p style={{ color: 'red' }}>{errors.phoneNumber}</p>
+              )}
+            </div>
+            <div className="form-group mt-3">
+              <label>Address</label>
+              <input
+                type="text"
+                onChange={handleFormChange}
+                value={formData.address.street}
+                className="form-control"
+                name="street"
+                placeholder="Street"
+              />
+              <input
+                type="text"
+                onChange={handleFormChange}
+                value={formData.address.city}
+                className="form-control mt-2"
+                name="city"
+                placeholder="City"
+              />
+              <input
+                type="text"
+                onChange={handleFormChange}
+                value={formData.address.state}
+                className="form-control mt-2"
+                name="state"
+                placeholder="State"
+              />
+              <input
+                type="text"
+                onChange={handleFormChange}
+                value={formData.address.postalCode}
+                className="form-control mt-2"
+                name="postalCode"
+                placeholder="Postal Code"
+              />
+            </div>
+            <button
+              type="submit"
+              onClick={handleFormSubmit}
+              className="btn btn-primary my-4"
+            >
+              Sign Up
+            </button>
+          </form>
+          <Link to={'/login'} style={{ textDecoration: 'none' }}>
+            Have an account?{' '}
+            <span style={{ textDecoration: 'underline' }}>Login in</span>
+          </Link>
+          <div>
+            <p style={{ color: 'green', fontSize: '1.2rem', paddingTop: '10px' }}>
+              {successMsg}
+            </p>
+          </div>
         </div>
-        <div className="form-group mt-3">
-          <label>Email</label>
-          <input
-            type="email"
-            onChange={handleFormChange}
-            className="form-control"
-            name="email"
-            value={formData.email}
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-          />
-          {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
-
-          <small id="emailHelp" className="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
-        </div>
-        <div className="form-group mt-3">
-          <label>Password</label>
-          <input
-            type="password"
-            onChange={handleFormChange}
-            value={formData.password}
-            className="form-control"
-            name="password"
-            placeholder="Password"
-          />
-          {errors.password && <p style={{ color: 'red' }}>{errors.password}</p>}
-        </div>
-        <div className="form-group mt-3">
-          <label>Phone Number</label>
-          <input
-            type="text"
-            onChange={handleFormChange}
-            value={formData.phoneNumber}
-            className="form-control"
-            name="phoneNumber"
-            placeholder="Phone"
-          />
-          {errors.phoneNumber && (
-            <p style={{ color: 'red' }}>{errors.phoneNumber}</p>
-          )}
-        </div>
-        <div className="form-group mt-3">
-          <label>Address</label>
-          <input
-            type="text"
-            onChange={handleFormChange}
-            value={formData.address.street}
-            className="form-control"
-            name="street"
-            placeholder="Street"
-          />
-          <input
-            type="text"
-            onChange={handleFormChange}
-            value={formData.address.city}
-            className="form-control mt-2"
-            name="city"
-            placeholder="City"
-          />
-          <input
-            type="text"
-            onChange={handleFormChange}
-            value={formData.address.state}
-            className="form-control mt-2"
-            name="state"
-            placeholder="State"
-          />
-          <input
-            type="text"
-            onChange={handleFormChange}
-            value={formData.address.postalCode}
-            className="form-control mt-2"
-            name="postalCode"
-            placeholder="Postal Code"
+        <div className="col-md-6 d-none d-md-flex align-items-center justify-content-center">
+          <img
+            src="https://media.wired.com/photos/61562228a62d729110ec10fe/4:3/w_1766,h_1324,c_limit/Gear-Meal-Kits-misfits-market-source-misfits-market.jpeg"
+            alt="Login Illustration"
+            className="img-fluid rounded"
           />
         </div>
-        <button
-          type="submit"
-          onClick={handleFormSubmit}
-          className="btn btn-primary my-4"
-        >
-          Sign Up
-        </button>
-      </form>
-      <Link to={'/login'} style={{ textDecoration: 'none' }}>
-        Have an account?{' '}
-        <span style={{ textDecoration: 'underline' }}>Login in</span>
-      </Link>
-      <div>
-        <p style={{ color: 'green', fontSize: '1.2rem', paddingTop: '10px' }}>
-          {successMsg}
-        </p>
       </div>
     </div>
   );
